e9a929101732b620a5aeef974702c5f3b0da5f0e
Aditya Gupta <iadityapansari@gmail.com>
2020-01-14 11:07:37 +0530

    cleans

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { selectSong } from '../actions';
 
 export default function App() {
+
     return (
         <div>
             App
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+ReactDOM.render(
+    <App />
+    , document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+{/* <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}></Provider> */ }
