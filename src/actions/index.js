860e76f4f2465538c083493729f9e9b53499cf8c
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 13:45:37 +0530

    adds reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 export const selectSong = song => {
   return {
-    type: "SONG_SELECT",
+    type: "SONG_SELECTED",
     payload: {
       song
     }
---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+export const songsReducer = () => {
+  return [
+    { title: "paradise", duration: "3:02" },
+    { title: "Hello", duration: "4:12" },
+    { title: "Fix You", duration: "5:25" }
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
