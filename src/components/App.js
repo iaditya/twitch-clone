244fa9819fadd92a13b54b8edbad9719d8706763
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 14:27:54 +0530

    renders song list

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,12 @@ import SongList from "../components/SongList";
 export default function App() {
   console.log("App renders");
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 }
