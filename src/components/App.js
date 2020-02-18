48952dd2097eabf4579db3f62fb0b8cf9d6e0664
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 11:29:39 +0530

    reset project to start again.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,6 @@
-import React from 'react';
-import SongList from './SongList';
-import SongDetail from './SongDetail';
+import React from "react";
 
 export default function App() {
-
-    console.log("App renders");
-
-    return (
-        <div className="ui container grid">
-            <div className="ui row">
-
-                <div className="column eight wide">
-                    <SongList />
-                </div>
-
-                <div className="column eight wide">
-                    <SongDetail />
-                </div>
-            </div>
-
-        </div>
-    )
+  console.log("App renders");
+  return <div>App</div>;
 }
