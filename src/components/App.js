b433473d976d3556e0c37e01e0d5d8a584a6fc1a
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 19:00:57 +0530

    adds posts comp.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import PostList from "./PostList";
 
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <PostList />
+      </div>
+    );
   }
 }
