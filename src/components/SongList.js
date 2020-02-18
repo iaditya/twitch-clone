8f58bd54acd05e510a291a16f35f959a2250daf1
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 15:26:23 +0530

    passing action-creators in redux using connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,13 @@ class SongList extends Component {
       return (
         <div className="item" key={song.duration}>
           <div className="right floated content">
-            <button className="ui button primary"> Select</button>
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              {" "}
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,12 +23,13 @@ class SongList extends Component {
   }
 
   render() {
+    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
-  return { songs: state.songs };
+  return { songs: state.songs, selectedSong: state.selectedSong };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
