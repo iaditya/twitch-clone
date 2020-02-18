2d6647b8743c22c45710e5d01d366f62c4d6762d
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 19:23:30 +0530

    adds axios instance

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-export const fetchPosts = () => {
+import jsonplaceholder from "../apis/jsonplaceholder";
+
+export const fetchPosts = async () => {
+  const response = await jsonplaceholder.get("/posts");
   return {
     type: "FETCH_POSTS"
   };
