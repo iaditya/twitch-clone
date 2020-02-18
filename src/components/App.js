4687e7b6f79f3a9e5314013988f7fcaf0a4d21c1
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-18 14:17:04 +0530

    use connect to get redux data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import SongList from "../components/SongList";
 
 export default function App() {
   console.log("App renders");
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 }
