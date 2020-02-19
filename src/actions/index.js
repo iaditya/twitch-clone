84efcf310cc74ccb83b38c5973a546941a6ab2a8
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-19 11:52:45 +0530

    hooks middleware for thunk, adds async action creator for fetch posts.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
 import jsonplaceholder from "../apis/jsonplaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get("/posts");
-  return {
-    type: "FETCH_POSTS"
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
