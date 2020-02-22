e8b2ea0373158c3e99d6741758bf82291493ea78
Aditya Gupta <aditya.gupta@2mee.com>
2020-02-22 16:28:45 +0530

    displays posts in UI

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,16 +6,27 @@ import { fetchPosts } from "../actions";
 class PostList extends Component {
   componentDidMount() {
     this.props.fetchPosts();
-    
   }
+
+  renderList = () => {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
 
   render() {
     console.log(this.props.posts);
-    return (
-      <div>
-        PostList: {this.props.posts ? this.props.posts.length : 0}
-      </div>
-    );
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
