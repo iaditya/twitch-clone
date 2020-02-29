97e6670977dfbbc59ee73764074656ab9d8f6e76
Aditya Gupta <iadityapansari@gmail.com>
2020-02-29 23:50:14 +0530

    reset project for twitch clone

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import PostList from "./PostList";
 
 export default class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <PostList />
+        Twitch Clone
       </div>
     );
   }
--- src/reducers/usersReducer.js
+++
@@ -1,9 +0,0 @@
-//FETCH_USER
-export default (state = [], action) => {
-  switch (action.type) {
-    case "FETCH_USER":
-        return [...state, action.payload];
-    default:
-      return state;
-  }
-};
