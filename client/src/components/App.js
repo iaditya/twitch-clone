9ad586c01a43aade221d026056b790dfd3919770
Aditya Gupta <aditya.gupta@2mee.com>
2020-03-01 23:04:37 +0530

    iniits components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
 
 function App() {
   return (
-    <div className="App">
-      twitch clone
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/create" component={StreamCreate} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/show" component={StreamShow} />
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
