8f77560638025b928a158cf7cfe7fe0c78d1192a
Aditya Gupta <aditya.gupta@2mee.com>
2020-03-21 19:12:52 +0530

    adds Header file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,18 +5,22 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 function App() {
   return (
-    <React.Fragment>
-      <BrowserRouter>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/create" component={StreamCreate} />
-        <Route path="/streams/delete" component={StreamDelete} />
-        <Route path="/streams/edit" component={StreamEdit} />
-        <Route path="/streams/show" component={StreamShow} />
-      </BrowserRouter>
-    </React.Fragment>
+    <div className="ui container">
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/create" component={StreamCreate} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/show" component={StreamShow} />
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className=" ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Stream
+        </Link>
+        <Link to="/" className="item">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
