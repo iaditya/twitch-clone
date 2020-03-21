48774733e40ee533526c08e80d213b9c1f435f9b
Aditya Gupta <iadityapansari@gmail.com>
2020-03-21 23:06:13 +0530

    updates auth state.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -16,16 +16,21 @@ export default class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.authChange);
         });
     });
   }
 
-  loginWithGoogle = () => {
-    window.gapi.auth2.getAuthInstance().signIn();
-    this.setState({
-      isSignedIn: window.gapi.auth2.getAuthInstance().isSignedIn.get()
-    });
+  authChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
+
+  //   loginWithGoogle = () => {
+  //     window.gapi.auth2.getAuthInstance().signIn();
+  //     this.setState({
+  //       isSignedIn: window.gapi.auth2.getAuthInstance().isSignedIn.get();
+  //     });
+  //   };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
@@ -33,7 +38,7 @@ export default class GoogleAuth extends Component {
     } else if (!!this.state.isSignedIn) {
       return <div>Logged IN</div>;
     } else {
-      return <div onClick={this.loginWithGoogle}>Login here</div>;
+      return <div>not Login </div>;
     }
   }
 
