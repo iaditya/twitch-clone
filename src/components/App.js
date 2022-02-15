89cf2a47839ef03f0c05e71c60b6a2b1705a3941
Aditya Pansari <iadityapansari@gmail.com>
2022-02-15 16:59:35 +0530

    removes old chnages, and reset the repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import SongList from './SongList';
-import SongDetail from './SongDetail';
-
+import React from "react";
 export default function App() {
-
-    console.log("App renders");
-
-    return (
-        <div className="ui container grid">
-            <div className="ui row">
-
-                <div className="column eight wide">
-                    <SongList />
-                </div>
-
-                <div className="column eight wide">
-                    <SongDetail />
-                </div>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="row">
+          <div className="col">Hello this is first div.</div>
+          <div className="col">Hello this is second div.</div>
+      </div>
+    </div>
+  );
 }
