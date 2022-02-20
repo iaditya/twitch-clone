c3554d99007558dc5be379a93e448b2c9bd03571
Aditya Pansari <iadityapansari@gmail.com>
2022-02-20 19:32:55 +0530

    adds login/logout functionalities

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function GoogleAuth() {
   const GOOGLE_CLIENT_ID =
     "75253675905-hl0mth765qqtl74sca722vq6r8c1aohi.apps.googleusercontent.com";
 
+  const [singnedIn, setSignedIn] = React.useState(null);
+  let auth = useRef(0);
+
   React.useEffect(() => {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({ clientId: GOOGLE_CLIENT_ID, scope: "email" });
+      window.gapi.client
+        .init({ clientId: GOOGLE_CLIENT_ID, scope: "email" })
+        .then(() => {
+          auth.current = window.gapi.auth2.getAuthInstance();
+          console.log("Auth", auth);
+          setSignedIn(auth.current.isSignedIn.get());
+          auth.current.isSignedIn.listen(() => {
+            setSignedIn(auth.current.isSignedIn.get());
+          });
+        });
     });
   }, []);
 
-  return <div>GoogleAuth</div>;
+  const signIn = () => auth.current.signIn();
+  const signOut = () => auth.current.signOut();
+
+  function renderAuthButton() {
+    if (singnedIn === null) {
+      return null;
+    } else if (singnedIn) {
+      return (
+        <button onClick={signOut} className="btn btn-secondary">
+          Sign out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={signIn} className="btn btn-primary">
+          <i className="fa-brands fa-google"></i> Sign in with Google
+        </button>
+      );
+    }
+  }
+
+  return <div>{renderAuthButton()}</div>;
 }
