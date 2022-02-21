09743494b616938ab2c24bef012aaf3a8b12b5b9
Aditya Pansari <iadityapansari@gmail.com>
2022-02-21 12:45:28 +0530

    enable redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import reducers from "./reducers";
 
+import reducers from "./reducers";
 import App from "./components/App";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
