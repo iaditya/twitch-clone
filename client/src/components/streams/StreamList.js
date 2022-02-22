b2a1dd429c9d24886fb21f5b970b1f74974c2166
Aditya Pansari <iadityapansari@gmail.com>
2022-02-22 17:19:43 +0530

    renders + styles streams list

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+const StreamList = ({ fetchStreams, streams }) => {
+  console.log(streams);
+  React.useEffect(() => {
+    console.log("StreamList use effect");
+    fetchStreams();
+  }, []);
+
+  const renderList = () => {
+    return streams.map((item) => {
+      return (
+        <div className="stream-card" key={item.id}>
+          <i className="fa-solid icon fa-camera"></i>
+
+          <div className="content">
+            <div className="name">{item.name}</div>
+            <div className="description">{item.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="streams">
+      <h2>Streams</h2>
+      <div className="stream-list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default StreamList;
+function mapsStateToProps(state) {
+  return { streams: Object.values(state.streams) };
+}
+
+export default connect(mapsStateToProps, { fetchStreams })(StreamList);
