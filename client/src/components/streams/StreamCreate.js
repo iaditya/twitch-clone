6e0c52a6b6ffa5f043b4d9968de0c3da95a9ff3c
Aditya Pansari <iadityapansari@gmail.com>
2022-02-23 17:51:12 +0530

    adds new From component so we can reuse on create & edit page.

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,67 +1,21 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError = (meta) => {
-    if (!!meta.touched)
-      return <small className="text-danger">{meta.error}</small>;
-  };
-
-  renderInput = (formProps) => {
-    return (
-      <div
-        className={`field ${
-          formProps.meta.touched && formProps.meta.error ? "error" : ""
-        }`}
-      >
-        <label>{formProps.label}</label>
-        <input autoComplete="off" type="text" {...formProps.input} />
-        {this.renderError(formProps.meta)}
-      </div>
-    );
-  };
-
   onSubmitHandler = (formValues) => {
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmitHandler)}
-        className="form"
-      >
-        <Field name="name" component={this.renderInput} label="Name" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Description"
-        />
-        <button className="btn btn-primary">Submit</button>
-      </form>
+      <div className="streams">
+        <h2>Create Stream</h2>
+        <StreamForm onSubmit={this.onSubmitHandler} />
+      </div>
     );
   }
 }
 
-const validateForms = (formValues) => {
-  let errors = {};
-  if (!formValues.name) errors.name = "Name is mandatory";
-  if (!formValues.description) errors.description = "Description is mandatory";
-
-  return errors;
-};
-
-// function mapStateToProps(state) {
-//   return {
-//     data: state,
-//   };
-// }
-
-const formWrapped = reduxForm({
-  form: "StreamCreate",
-  validate: validateForms,
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError = (meta) => {
+    if (!!meta.touched)
+      return <small className="text-danger">{meta.error}</small>;
+  };
+
+  renderInput = (formProps) => {
+    return (
+      <div
+        className={`field ${
+          formProps.meta.touched && formProps.meta.error ? "error" : ""
+        }`}
+      >
+        <label>{formProps.label}</label>
+        <input autoComplete="off" type="text" {...formProps.input} />
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  onSubmitHandler = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmitHandler)}
+        className="form"
+      >
+        <Field name="name" component={this.renderInput} label="Name" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Description"
+        />
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validateForms = (formValues) => {
+  let errors = {};
+  if (!formValues.name) errors.name = "Name is mandatory";
+  if (!formValues.description) errors.description = "Description is mandatory";
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "StreamForm",
+  validate: validateForms,
+})(StreamForm);
