7d4101defc37c9bd080b294149d5977b85c82a3b
Aditya Pansari <iadityapansari@gmail.com>
2022-03-03 15:12:21 +0530

    stops event bubbling stopPropagation()

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="modal">
-      <div className="modal-dialog">
+    <div onClick={() => history.push("/")} className="modal">
+      <div onClick={(e) => e.stopPropagation()} className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
             <h3>Delete Stream</h3>
           </div>
           <div className="modal-body">
-            <p>Are you sure to delete this stream - name</p>
+            <p>Are you sure you want to delete this stream - name</p>
           </div>
           <div className="modal-footer">
             <button className="btn btn-primary">Confirm</button>
