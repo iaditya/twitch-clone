7a0110d1957e75b7a54ac1ca4aa9ca3ab81fbb76
Aditya Pansari <iadityapansari@gmail.com>
2022-03-06 19:27:31 +0530

    obs installation on machine, use live url to stream - use stream id for obs stream key .

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 const StreamShow = ({ match, fetchStream, stream }) => {
+  const videoRef = useRef(null);
+  const streamFetched = !!stream && stream.id;
+  let id = match.params.id;
   useEffect(() => {
-    fetchStream(match.params.id);
-  }, [match.params.id, fetchStream]);
+    fetchStream(id);
+
+    if (streamFetched) {
+      console.log("makes player");
+      let player = flv.createPlayer({
+        type: "flv",
+        url: `http://localhost:8000/live/${id}.flv`,
+      });
+      player.attachMediaElement(videoRef.current);
+      player.load();
+    }
+  }, [id, fetchStream, streamFetched]);
 
   if (!stream) {
     return <div>Loading..</div>;
@@ -13,6 +27,7 @@ const StreamShow = ({ match, fetchStream, stream }) => {
   const { name, description } = stream;
   return (
     <div className="streams">
+      <video style={{ width: "100%" }} controls ref={videoRef} />
       <h2>{name}</h2>
       <p>{description}</p>
     </div>
